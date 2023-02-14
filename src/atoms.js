867d5c75add6badea6b5108ab7cb74afae6eada1
basdzxqwe <ock6166@naver.com>
2023-02-14 19:51:01 +0900

    할 일 추가

---
+++ src/atoms.js
@@ -0,0 +1,20 @@
+import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+export const { persistAtom: persistAtomTodos } = recoilPersist({
+    key: "persistAtomTodos"
+});
+export const { persistAtom: persistAtomLastTodoId } = recoilPersist({
+    key: "persistAtomLastTodoId"
+});
+export const todosAtom = atom({
+    key: "app/todosAtom",
+    default: [],
+    effects_UNSTABLE: [persistAtomTodos],
+});
+
+export const lastTodoIdAtom = atom({
+    key: "app/lastTodoIdAtom",
+    default: 0,
+    effects_UNSTABLE: [persistAtomLastTodoId],
+});
--- src/pages/WritePage.js
+++ src/pages/WritePage.js
@@ -1,18 +1,50 @@
-import { TextField } from "@mui/material";
+import { TextField, Button } from "@mui/material";
+import { useTodosStatus } from "../hooks";
 
 export default function WritePage() {
+  const todosStatus = useTodosStatus();
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+
+    if (form.regDate.value.length == 0) {
+      alert("날짜를 입력해주세요.");
+      form.regDate.focus();
+
+      return;
+    }
+
+    if (form.regDate.content.length == 0) {
+      alert("내용을 입력해주세요.");
+      form.regDate.focus();
+
+      return;
+    };
+
+    todosStatus.addTodo(form.regDate.value, form.content.value)
+  }
+
   return (
     <>
-      <div className="flex-1 flex p-10 flex-col gap-7">
-          <TextField label="언제 해야 하나요?" focused type="datetime-local" />
-          <TextField 
-          label="무엇을 해야 하나요?" 
-          className="flex-1 flex" 
-          InputProps={{className : "flex-1 flex-col"}} 
-          inputProps={{className : "flex-1"}}
+      <form className="flex-1 flex p-10 flex-col gap-7" onSubmit={onSubmit}>
+        <TextField label="언제 해야 하나요?" focused type="datetime-local" name="regDate" />
+        <TextField
+          name="content"
+          label="무엇을 해야 하나요?"
+          className="flex-1 flex"
+          InputProps={{ className: "flex-1 flex-col" }}
+          inputProps={{ className: "flex-1" }}
           multiline
-          />
-      </div>
+        />
+      </form>
+      <Button type="submit" variant="contained">
+        <span>
+          <i class="fa-regular fa-pen-to-square"></i>
+        </span>
+        <span>&nbsp; &nbsp;</span>
+        <span>할 일 추가</span>
+      </Button>
     </>
   );
 }
